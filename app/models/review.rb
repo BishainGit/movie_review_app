0c50ae344c3107eaa6c882bc23f1916b3e86ddf9
BISHAIN <bishain1997@gmail.com>
2019-04-18 18:05:10 +0530

    updated schema

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to:user
+  bilogns_to:movie
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_200145) do
+ActiveRecord::Schema.define(version: 2019_04_15_202543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2019_04_15_200145) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
